dbd1ba52d7e888573d5f8066a22775729a135679
daniel <danieldondorp@gmail.com>
2024-03-22 12:13:00 +0100

    Solve truncatable primes

--- content/daniels_solutions/utils.py
+++ content/daniels_solutions/utils.py
@@ -35,7 +35,10 @@ def get_common_denominator(a, b):
     return get_common_denom(b, a%b)
 
 def is_prime(n):
-    for i in range(2, int(np.sqrt(np.abs(n)))+1):
-        if n%i==0:
-            return False
+    if n<=1:
+        return False
+    else:
+        for i in range(2, int(np.sqrt(np.abs(n)))+1):
+            if n%i==0:
+                return False
     return True
